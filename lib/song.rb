27cbd048394ff379a400740e2b34f8480561795b
Zach Kobrinsky <zachkobrinsky@gmail.com>
2020-08-10 20:58:38 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,6 @@
 class Song
 
-  attr_accessor :artist, :name
+  attr_accessor :artist, :name, :genre
 
   @@all = []
 
@@ -13,12 +13,31 @@ class Song
     @@all
   end
 
+  # def self.new_by_filename(filename)
+  #   @name = filename.split(" - ")[1]
+  #   @artist = Artist.new(filename.split(" - ")[0])
+  #   song = self.new(@name)
+  #   song.artist = @artist
+  #   # genre = filename.split(" - ")[2]
+  #   # @genre = genre.split(".")[0]
+  #   # song.genre = @genre
+  #   @@all << song
+  #   song
+  #   binding.pry
+  #
+  #   # if Artist.all.detect{|song| song.name == @name}
+  #
+  #   # 5) Song .new_by_filename associates new song instance with the artist from the filename
+  #   # Failure/Error: expect(Artist.all.first.songs.empty?).to eq(false)
+  #   #   expected: false
+  #   #        got: true
+  # end
+
   def self.new_by_filename(filename)
-    # file_name = 'Michael Jackson - Black or White - pop.mp3'
-    filename.split(" - ")
-    # song = self.new(song)
-    # song.artist = song
-  end
-
-
+    @name = filename.split(" - ")[1]
+    @artist = Artist.new(filename.split(" - ")[0])
+    song = self.new(@name)
+    song.artist = @artist
+    @@all << song
+    song
 end
